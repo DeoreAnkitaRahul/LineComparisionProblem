7d01c4710b76856d7145fb06e9a322e02849690a
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-04-25 15:23:27 +0530

    [Ankita] Add : UC1-Calculate Length

---
+++ LineComparisionProblem/LineComparisionProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿using LineComparisionProblem;
+
+namespace LineComparision
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            CalculateLength obj = new CalculateLength(20, 30, 40, 50);
+            obj.Calculate();
+            Console.ReadKey();
+        }
+    }
+}
